d6051732cb3b4c8194e8ffb240b633261ec0d380
Misbah Uddin <rayvi73@gmail.com>
2023-06-06 23:22:40 +0600

    added basic router set up

--- src/Routes/Routers/Routers.jsx
+++ src/Routes/Routers/Routers.jsx
@@ -1,10 +1,19 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Main from "../../Layout/Main/Main";
+import Home from "../../Pages/Home/Home";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
 
-const Routers = () => {
-    return (
-        <div>
-            <h1>This is router page</h1>
-        </div>
-    );
-};
-
-export default Routers;
+export default router;
